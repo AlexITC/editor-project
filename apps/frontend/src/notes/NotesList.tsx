6d7c80f036be5f385f6448113ab1a3e302fb3183
Alexis Hernandez <alexis22229@gmail.com>
2021-11-29 02:28:35 -0700

    frontend: Allow creating new notes
    While the action works, the changes aren't propagated as expected
    because the websocket connection is drop frequently but reconnecting
    is missing.

--- apps/frontend/src/notes/NotesList.tsx
+++ apps/frontend/src/notes/NotesList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import TextField from '@mui/material/TextField';
 import { Assignment as AssignmentIcon } from '@mui/icons-material'
 import { useNotesList } from './hooks'
 
@@ -10,6 +11,32 @@ interface NotesListProps {
 
 const NotesList: React.FC<NotesListProps> = ({ activeNoteId }) => {
   const { notes, create, update } = useNotesList()
+  const [newName, setNewName] = useState<string>('')
+  const [creationError, setCreationError] = useState<string>('')
+  const [creating, setCreating] = useState<Boolean>(false)
+  const submit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (newName.trim().length === 0) {
+      setCreationError('A non-blank name is required')
+      return;
+    }
+
+    setCreating(true)
+    setCreationError('')
+    try {
+      create(newName)
+    } catch (e) {
+      setCreationError('Unexpected error')
+    } finally {
+      setCreating(false)
+    }
+  }
+
+  const handleNewNameChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const newValue = e.target.value
+    setNewName(newValue)
+    setCreationError('')
+  }
 
   return (
     <List>
@@ -23,6 +50,16 @@ const NotesList: React.FC<NotesListProps> = ({ activeNoteId }) => {
           </ListItemButton>
         </Link>
       ))}
+      <form onSubmit={submit}>
+        <TextField
+            required
+            label="New note"
+            value={newName}
+            error={creationError.length > 0}
+            helperText={creationError}
+            onChange={handleNewNameChange}
+          />
+      </form>
     </List>
   )
 }
