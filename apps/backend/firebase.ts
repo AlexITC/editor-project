3a520746576a669f17f1b8b487ff0230f2714000
Alexis Hernandez <alexis22229@gmail.com>
2021-11-28 17:51:32 -0700

    Update the firebase class
    There were some changes required in order to properly use the firebase datastore.

--- apps/backend/firebase.ts
+++ apps/backend/firebase.ts
@@ -1,11 +1,14 @@
-import admin from 'firebase-admin'
+import { ServiceAccount } from 'firebase-admin'
+import app from 'firebase-admin';
+import firestore from 'firebase-admin';
 
 // Replace this file with your service account key you get when setting up a firestore.
 import serviceAccount from './serviceAccountKey.json'
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+console.log('initialize app');
+app.initializeApp({
+  credential: app.credential.cert(serviceAccount as ServiceAccount),
   databaseURL: "https://databaseName.firebaseio.com"
 })
 
-export default admin.database()
+export default firestore.firestore()
